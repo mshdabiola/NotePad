f265de666cc199c9b9da76514888bb1675ebff57
mshabiola <mshdabiola@gmal.com>
2022-12-16 12:05:09 +0100

    change layout of NoteCard

--- feature/mainscreen/src/main/java/com/mshdabiola/mainscreen/component/NoteCard.kt
+++ feature/mainscreen/src/main/java/com/mshdabiola/mainscreen/component/NoteCard.kt
@@ -1,15 +1,17 @@
 package com.mshdabiola.mainscreen.component
 
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.mshdabiola.mainscreen.state.NoteUiState
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -19,31 +21,20 @@ fun NoteCard(noteUiState: NoteUiState, onCardClick: (Long) -> Unit = {}) {
     OutlinedCard(
         modifier = Modifier,
         onClick = { noteUiState.id?.let { onCardClick(it) } }) {
-//        Column(Modifier.padding(16.dp)) {
-//            Text(text = noteUiState.title, style = MaterialTheme.typography.titleMedium)
-//            Spacer(modifier = Modifier.height(8.dp))
-//            Text(text = noteUiState.detail, style = MaterialTheme.typography.bodyMedium)
-//        }
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            ListItem(
-                headlineText = {
-                    Text(text = noteUiState.title.ifEmpty { noteUiState.detail })
-                },
-                supportingText = {
-                    if (noteUiState.title.isNotEmpty()) {
-                        if (noteUiState.detail.isNotEmpty()) {
-                            Text(
-                                text = noteUiState.detail,
-
-                                maxLines = 1,
-                                overflow = TextOverflow.Ellipsis
-                            )
-                        }
-                    }
-                },
-                modifier = Modifier.weight(1f)
+        Column(Modifier.padding(16.dp)) {
+            Text(
+                text = noteUiState.title.ifEmpty { noteUiState.detail },
+                style = MaterialTheme.typography.titleMedium
             )
+
+            if (noteUiState.title.isNotEmpty()) {
+                if (noteUiState.detail.isNotEmpty()) {
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(text = noteUiState.detail, style = MaterialTheme.typography.bodyMedium)
+                }
+            }
         }
+
     }
 }
 
