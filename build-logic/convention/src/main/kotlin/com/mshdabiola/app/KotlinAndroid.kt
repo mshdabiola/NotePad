ec9681d9d98354ca1e3548c2759c553162be9b00
mshabiola <mshdabiola@gmal.com>
2023-01-18 01:15:22 +0100

    build : upgrade version and android studio

--- build-logic/convention/src/main/kotlin/com/mshdabiola/app/KotlinAndroid.kt
+++ build-logic/convention/src/main/kotlin/com/mshdabiola/app/KotlinAndroid.kt
@@ -37,13 +37,13 @@ internal fun Project.configureKotlinAndroid(
         }
 
         compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_1_8
-            targetCompatibility = JavaVersion.VERSION_1_8
+            sourceCompatibility = JavaVersion.VERSION_17
+            targetCompatibility = JavaVersion.VERSION_17
         }
 
         kotlinOptions {
             // Set JVM target to 1.8
-            jvmTarget = JavaVersion.VERSION_1_8.toString()
+            jvmTarget = JavaVersion.VERSION_17.toString()
 
         }
     }
