64a9eed2591bd4b445bb48ceaec577f30f0fbf94
mshabiola <mshdabiola@gmal.com>
2023-01-22 16:03:45 +0100

    build: release/0.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
             // who clones the code to sign and run the release variant, use the debug signing key.
             // TODO: Abstract the signing configuration to a separate file to avoid hardcoding this.
             // Todo: comment code before release
-             signingConfig = signingConfigs.getByName("debug")
+//              signingConfig = signingConfigs.getByName("debug")
         }
         val benchmark by creating {
             // Enable all the optimizations from release build through initWith(release).
