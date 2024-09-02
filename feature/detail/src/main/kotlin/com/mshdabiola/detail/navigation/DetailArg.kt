2b10d1eec258a46ba2cdbf4d6a876b3937db261f
mshdabiola <mshdabiola@gmail.com>
2024-09-02 16:14:20 +0100

    feat(detail): Simplify Detail Navigation
    Removed unused imports and simplified the `DetailNavigation` function.
    Notably, removed the use of `navArgument
    ` and `NavType` as they were unnecessary for the current navigation setup.
    Also, removed `SavedStateHandle` and `VisibleForTesting` annotation as they were not being utilized.

--- feature/detail/src/main/kotlin/com/mshdabiola/detail/navigation/DetailArg.kt
+++ feature/detail/src/main/kotlin/com/mshdabiola/detail/navigation/DetailArg.kt
@@ -3,4 +3,4 @@ package com.mshdabiola.detail.navigation
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class DetailArg (val id:Long)
+data class DetailArg(val id: Long)
--- feature/detail/src/main/kotlin/com/mshdabiola/detail/navigation/DetailNavigation.kt
+++ feature/detail/src/main/kotlin/com/mshdabiola/detail/navigation/DetailNavigation.kt
@@ -4,14 +4,10 @@
 
 package com.mshdabiola.detail.navigation
 
-import androidx.annotation.VisibleForTesting
-import androidx.lifecycle.SavedStateHandle
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
-import androidx.navigation.NavType
 import androidx.navigation.compose.composable
-import androidx.navigation.navArgument
 import com.mshdabiola.detail.DetailRoute
 
 fun NavController.navigateToDetail(detailArg: DetailArg, navOptions: NavOptions = androidx.navigation.navOptions { }) = navigate(detailArg, navOptions)
