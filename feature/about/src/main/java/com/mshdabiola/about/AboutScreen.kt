7812bf760cc82fe2ad497273997b5ee086650192
mshdabiola <mshdabiola@gmail.com>
2024-10-17 10:05:23 +0100

    fix(about): Use AutoMirrored.Filled.ArrowBack for navigation icon
    Updated the navigation icon in the About screen to use `Icons.AutoMir
    rored.Filled.ArrowBack` instead of `Icons.Default.ArrowBack`.
    
    This ensures that the arrow icon is automatically mirrored in right-to-left (RTL) layouts, providing a better user experience for users in those locales.

--- feature/about/src/main/java/com/mshdabiola/about/AboutScreen.kt
+++ feature/about/src/main/java/com/mshdabiola/about/AboutScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
@@ -60,7 +61,7 @@ fun AboutScreen(
             TopAppBar(
                 navigationIcon = {
                     IconButton(onClick = onBack) {
-                        Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "back")
+                        Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "back")
                     }
                 },
                 title = {
