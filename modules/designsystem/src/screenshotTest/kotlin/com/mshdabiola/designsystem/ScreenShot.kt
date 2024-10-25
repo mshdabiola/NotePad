d814185200d753f409b1f59c1ceeec488817686a
mshdabiola <mshdabiola@gmail.com>
2024-10-25 12:03:56 +0100

    refactor(designsystem): Update resource organization
    This commit updates the organization of resources within the design system module by renaming and removing files.
    
    - Resources related
     to features like `main_search_sort` and `mainscreen_note_taking` were renamed to use the `modules_designsystem` prefix for better clarity and consistency.
    - Strings and font certificates were also updated to align with this naming convention.
    - Some redundant or unused resource files were removed to
     reduce clutter and improve maintainability.
    - Updated the `TopAppBarScreenShot` to use icons from `NoteIcon` instead of `SkIcons` and string resources for better consistency.
    - Added a new string resource `modules_designsystem_settings`.
    
    This change aims to improve the structure and maintainability of the design system module's resources.

--- modules/designsystem/src/screenshotTest/kotlin/com/mshdabiola/designsystem/ScreenShot.kt
+++ modules/designsystem/src/screenshotTest/kotlin/com/mshdabiola/designsystem/ScreenShot.kt
@@ -2,19 +2,20 @@ package com.mshdabiola.designsystem
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.mshdabiola.designsystem.component.SkTopAppBar
-import com.mshdabiola.designsystem.icon.SkIcons
+import com.mshdabiola.designsystem.icon.NoteIcon
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
 private fun TopAppBarScreenShot() {
     SkTopAppBar(
-        titleRes = android.R.string.untitled,
-        navigationIcon = SkIcons.Search,
+        titleRes = stringResource(android.R.string.untitled),
+        navigationIcon = NoteIcon.Search,
         navigationIconContentDescription = "Navigation icon",
-        actionIcon = SkIcons.MoreVert,
+        actionIcon = NoteIcon.MoreVert,
         actionIconContentDescription = "Action icon",
     )
 }
