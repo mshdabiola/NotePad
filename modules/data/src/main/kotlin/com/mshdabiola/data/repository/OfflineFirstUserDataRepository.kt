7554399c3f588ad6cf5a42092c8cd58ab4583a00
mshdabiola <mshdabiola@gmail.com>
2024-10-28 12:08:14 +0100

    refactor(data): Migrate to UserPreferencesRepository
    Migrated from `SkPreferencesDataSource` to `UserPreferencesRepository` in `OfflineFirstUserDataRepository`.
    
    - Updated `OfflineFirstUserDataRepository` to use `UserPreferencesRepository` for data access.
    - Updated tests to use `UserPreferencesRepository` instead of `SkPreferencesDataSource`.

--- modules/data/src/main/kotlin/com/mshdabiola/data/repository/OfflineFirstUserDataRepository.kt
+++ modules/data/src/main/kotlin/com/mshdabiola/data/repository/OfflineFirstUserDataRepository.kt
@@ -5,7 +5,7 @@
 package com.mshdabiola.data.repository
 
 import com.mshdabiola.analytics.AnalyticsHelper
-import com.mshdabiola.datastore.SkPreferencesDataSource
+import com.mshdabiola.datastore.UserPreferencesRepository
 import com.mshdabiola.model.Contrast
 import com.mshdabiola.model.DarkThemeConfig
 import com.mshdabiola.model.ThemeBrand
@@ -14,35 +14,35 @@ import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 internal class OfflineFirstUserDataRepository @Inject constructor(
-    private val skPreferencesDataSource: SkPreferencesDataSource,
+    private val userPreferencesRepository: UserPreferencesRepository,
     private val analyticsHelper: AnalyticsHelper,
 ) : UserDataRepository {
 
     override val userData: Flow<UserData> =
-        skPreferencesDataSource.userData
+        userPreferencesRepository.userData
 
     override suspend fun setThemeBrand(themeBrand: ThemeBrand) {
-        skPreferencesDataSource.setThemeBrand(themeBrand)
+        userPreferencesRepository.setThemeBrand(themeBrand)
         analyticsHelper.logThemeChanged(themeBrand.name)
     }
 
     override suspend fun setThemeContrast(contrast: Contrast) {
-        skPreferencesDataSource.setThemeContrast(contrast)
+        userPreferencesRepository.setThemeContrast(contrast)
         analyticsHelper.logContrastChanged(contrast.name)
     }
 
     override suspend fun setDarkThemeConfig(darkThemeConfig: DarkThemeConfig) {
-        skPreferencesDataSource.setDarkThemeConfig(darkThemeConfig)
+        userPreferencesRepository.setDarkThemeConfig(darkThemeConfig)
         analyticsHelper.logDarkThemeConfigChanged(darkThemeConfig.name)
     }
 
     override suspend fun setDynamicColorPreference(useDynamicColor: Boolean) {
-        skPreferencesDataSource.setDynamicColorPreference(useDynamicColor)
+        userPreferencesRepository.setDynamicColorPreference(useDynamicColor)
         analyticsHelper.logDynamicColorPreferenceChanged(useDynamicColor)
     }
 
     override suspend fun setShouldHideOnboarding(shouldHideOnboarding: Boolean) {
-        skPreferencesDataSource.setShouldHideOnboarding(shouldHideOnboarding)
+        userPreferencesRepository.setShouldHideOnboarding(shouldHideOnboarding)
         analyticsHelper.logOnboardingStateChanged(shouldHideOnboarding)
     }
 }
