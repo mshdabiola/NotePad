9749081302db10b403911126a622ea4e088116a7
mshdabiola <mshdabiola@gmail.com>
2025-06-10 19:28:52 +0100

    Refactor: Align MainData.Label index with its ID
    Updated the `MainData.Label` class to use its `id` property as its `index`. Previously, it was hardcoded to `0L`.
    
    - `MainData.Label(val id: Long) : MainData(0L)` -> `MainData.Label(val id: Long) : MainData(index = id)` in `MainData.kt`

--- modules/model/src/main/java/com/mshdabiola/model/MainData.kt
+++ modules/model/src/main/java/com/mshdabiola/model/MainData.kt
@@ -4,6 +4,6 @@ sealed class MainData(val index: Long) {
     object Note : MainData(-1L)
     object Achieve : MainData(-2L)
     object Trash : MainData(-3L)
-    data class Label(val id: Long) : MainData(0L) // Assigning a default index like in the original enum
+    data class Label(val id: Long) : MainData(index = id) // Assigning a default index like in the original enum
     object Remainder : MainData(-4L)
 }
