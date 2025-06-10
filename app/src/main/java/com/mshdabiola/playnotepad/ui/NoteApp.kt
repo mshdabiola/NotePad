14b6b66bbd7a2b6a0f6e1d2d066f10fda998b08d
mshdabiola <mshdabiola@gmail.com>
2025-06-10 11:52:58 +0100

    Refactor: Remove unused import in NoteApp
    Removed the `androidx.navigation.navOptions` import from `NoteApp.kt` as it was no longer needed.

--- app/src/main/java/com/mshdabiola/playnotepad/ui/NoteApp.kt
+++ app/src/main/java/com/mshdabiola/playnotepad/ui/NoteApp.kt
@@ -41,7 +41,6 @@ import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTagsAsResourceId
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.navigation.navOptions
 import com.mshdabiola.about.navigateToAbout
 import com.mshdabiola.designsystem.component.NoteBackground
 import com.mshdabiola.designsystem.component.NoteGradientBackground
