295ab35fe1a44f90cefdd1f4e16270734f11c135
mshdabiola <mshdabiola@gmail.com>
2025-06-10 09:27:05 +0100

    Refactor: Add trailing comma in UserData.kt
    Added a trailing comma to the `mainData` property in the `UserData` data class. This is a stylistic change for consistency.
    
    Also, removed an extra newline from the end of `MainData.kt`.

--- modules/model/src/main/java/com/mshdabiola/model/MainData.kt
+++ modules/model/src/main/java/com/mshdabiola/model/MainData.kt
@@ -1,11 +1,9 @@
 package com.mshdabiola.model
 
-
 sealed class MainData(val index: Long) {
     object Note : MainData(-1L)
     object Achieve : MainData(-2L)
     object Trash : MainData(-3L)
-    data class Label (val id: Long): MainData(0L) // Assigning a default index like in the original enum
+    data class Label(val id: Long) : MainData(0L) // Assigning a default index like in the original enum
     object Remainder : MainData(-4L)
-
 }
