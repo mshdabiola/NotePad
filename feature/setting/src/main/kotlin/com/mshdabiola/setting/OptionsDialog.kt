eb8a819638d4b124efeec7f9edec19faf53a6cd5
mshdabiola <mshdabiola@gmail.com>
2025-06-11 08:01:29 +0100

    Refactor: Update OptionsDialog to use NoteTextButton
    This commit updates the `OptionsDialog` composable in `OptionsDialog.kt` to use the `NoteTextButton` component from the design system module for its confirm button.
    
    - Replaced `TextButton` with `NoteTextButton` for the dialog's confirm button.

--- feature/setting/src/main/kotlin/com/mshdabiola/setting/OptionsDialog.kt
+++ feature/setting/src/main/kotlin/com/mshdabiola/setting/OptionsDialog.kt
@@ -7,13 +7,13 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.fastForEachIndexed
+import com.mshdabiola.designsystem.component.NoteTextButton
 import com.mshdabiola.designsystem.R as Rd
 
 @Composable
@@ -28,7 +28,7 @@ fun OptionsDialog(
         modifier = modifier,
         onDismissRequest = onDismiss,
         confirmButton = {
-            TextButton(onClick = onDismiss) {
+            NoteTextButton(onClick = onDismiss) {
                 Text(stringResource(Rd.string.modules_designsystem_close))
             }
         },
