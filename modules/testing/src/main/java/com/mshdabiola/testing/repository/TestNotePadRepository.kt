9c8337e9800e9980397021624194c849c924dada
mshdabiola <mshdabiola@gmail.com>
2025-06-13 14:01:06 +0100

    Refactor: Update `getOneNotePad` in test repositories
    This commit updates the `getOneNotePad` function in both `TestNotePadRepository` and `FakeNotePadRepository` to return a `NotePad` object with the provided `id`.
    
    - In `TestNotePadRepository.kt`:
        - Modified `getOneNotePad(id: Long)` to return `flow { NotePad(id = id) }`.
        - Added a `println` statement for debugging.
    - In `FakeNotePadRepository.kt`:
        - Modified `getOneNotePad(id: Long)` to return `flow { NotePad(id = id) }`.
        - Added a `println` statement for debugging.

---
+++ modules/testing/src/main/java/com/mshdabiola/testing/repository/TestNotePadRepository.kt
@@ -0,0 +1,93 @@
+package com.mshdabiola.testing.repository
+
+import com.mshdabiola.data.repository.INotePadRepository
+import com.mshdabiola.model.NotePad
+import com.mshdabiola.model.NoteType
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.LocalTime
+
+class TestNotePadRepository : INotePadRepository {
+    override suspend fun upsert(notePad: NotePad): Long {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun upsert(notePads: List<NotePad>) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteCheckNote(id: Long, noteId: Long) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteNoteCheckByNoteId(noteId: Long) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getNotePads(noteType: NoteType): Flow<List<NotePad>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getNotePads(): Flow<List<NotePad>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getOneNotePad(id: Long): Flow<NotePad?> {
+        println("id1 $id")
+
+        return flow { NotePad(id = id) }
+    }
+
+    override suspend fun deleteTrashType() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteNotePad(notePads: List<NotePad>) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun delete(notePads: List<NotePad>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun timeToString(time: LocalTime): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun dateToString(date: LocalDate): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun dateToString(long: Long): String {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteVoiceNote(id: Long) {
+        TODO("Not yet implemented")
+    }
+
+    override fun saveImage(uri: String): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun saveVoice(uri: String): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun getUri(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun getVoicePath(id: Long): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun getImagePath(id: Long): String {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteImageNote(id: Long) {
+        TODO("Not yet implemented")
+    }
+}
