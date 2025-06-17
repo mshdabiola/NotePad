f8c2f2bd1bab3c3f9be79889dfb9a8021d3f6bfd
mshdabiola <mshdabiola@gmail.com>
2025-06-18 00:28:21 +0100

    add buttons

--- modules/ui/src/main/kotlin/com/mshdabiola/ui/NoteficationDialog.kt
+++ modules/ui/src/main/kotlin/com/mshdabiola/ui/NoteficationDialog.kt
@@ -42,6 +42,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -492,7 +493,10 @@ fun DateTextDropbox(
             readOnly = true,
             state = state,
             trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
-            colors = ExposedDropdownMenuDefaults.textFieldColors(),
+            colors = ExposedDropdownMenuDefaults.textFieldColors(
+                focusedContainerColor = Color.Transparent,
+                unfocusedContainerColor = Color.Transparent
+            ),
             lineLimits = TextFieldLineLimits.SingleLine,
 
         )
