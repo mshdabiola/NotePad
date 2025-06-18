98ac9e6362f1249691a7b9fefcf394cde645e633
mshdabiola <mshdabiola@gmail.com>
2025-06-18 17:46:59 +0100

    Fix: Correct "REMAINDER" typo to "REMINDER" in NoteType
    This commit corrects a typo in the `NoteType` enum, changing `REMAINDER` to `REMINDER`.
    
    Additionally, it refactors the `NoteDisplayCategory` data class.
    
    **Key changes:**
    
    -   **`modules/model/src/main/java/com/mshdabiola/model/NoteType.kt`**:
        -   Renamed `NoteType.REMAINDER` to `NoteType.REMINDER`.
    -   **`modules/model/src/main/java/com/mshdabiola/model/NoteDisplayCategory.kt`**:
        -   Removed the `index` property and the logic that derived `noteType` from `index`.
        -   Added a `labelId` property with a default value of `1`.
        -   Added a `noteType` property of type `NoteType` with a default value of `NoteType.NOTE`.

--- modules/model/src/main/java/com/mshdabiola/model/NoteDisplayCategory.kt
+++ modules/model/src/main/java/com/mshdabiola/model/NoteDisplayCategory.kt
@@ -1,12 +1,6 @@
 package com.mshdabiola.model
 
-data class NoteDisplayCategory(val index: Long = NoteType.NOTE.index) {
-    val noteType: NoteType
-        get() = when (index) {
-            -1L -> NoteType.NOTE
-            -2L -> NoteType.ARCHIVE
-            -3L -> NoteType.TRASH
-            -4L -> NoteType.REMAINDER
-            else -> NoteType.LABEL
-        }
-}
+data class NoteDisplayCategory(
+    val labelId: Long = 1,
+    val noteType: NoteType = NoteType.NOTE,
+)
