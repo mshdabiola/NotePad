8fc4445d9af69949b36439eb2b3cceab24f6400c
mshdabiola <mshdabiola@gmail.com>
2025-06-20 01:08:13 +0100

    Refactor: Introduce `Select` state to `SearchState`
    This commit refactors the `SearchState` sealed class by introducing a new `Select` state.
    
    - **`feature/search/src/main/kotlin/com/mshdabiola/search/SearchState.kt`**:
        - Added a new data class `Select` to `SearchState`. This state will hold lists of `SearchSort.Type`, `SearchSort.Color`, and `SearchSort.Label`.
        - Moved `types`, `color`, and `label` properties from the `Success` state to the new `Select` state.
        - Added an `isGrid` boolean property to the `Success` state.

--- feature/search/src/main/kotlin/com/mshdabiola/search/SearchState.kt
+++ feature/search/src/main/kotlin/com/mshdabiola/search/SearchState.kt
@@ -5,11 +5,16 @@ import com.mshdabiola.model.NotePad
 sealed class SearchState {
     data object Loading : SearchState()
 
-    data class Success(
-        val searches: List<NotePad> = emptyList(),
+    data class Select(
+
         val types: List<SearchSort.Type> = emptyList(),
         val color: List<SearchSort.Color> = emptyList(),
         val label: List<SearchSort.Label> = emptyList(),
+    ) : SearchState()
+
+    data class Success(
+        val searches: List<NotePad> = emptyList(),
+        val isGrid: Boolean = false,
         val searchSort: SearchSort? = null,
 
     ) : SearchState()
