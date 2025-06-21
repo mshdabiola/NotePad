9bfb925cbe222ec75c31680eccdefb112549f4db
mshdabiola <mshdabiola@gmail.com>
2025-06-21 13:13:48 +0100

    Refactor: Remove unused imports in LabelViewModel
    This commit removes unused imports (`androidx.compose.runtime.getValue`, `androidx.compose.runtime.setValue`, and `kotlinx.coroutines.flow.map`) from `LabelViewModel.kt`.
    
    - **`feature/labelscreen/src/main/java/com/mshdabiola/labelscreen/LabelViewModel.kt`**:
        - Removed unused import statements.

--- feature/labelscreen/src/main/java/com/mshdabiola/labelscreen/LabelViewModel.kt
+++ feature/labelscreen/src/main/java/com/mshdabiola/labelscreen/LabelViewModel.kt
@@ -1,7 +1,5 @@
 package com.mshdabiola.labelscreen
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -16,7 +14,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
