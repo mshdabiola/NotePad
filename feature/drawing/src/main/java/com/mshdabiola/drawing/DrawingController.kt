d027cc517527f0549f74cd9646fecc18d9b3d620
mshdabiola <mshdabiola@gmail.com>
2025-06-22 20:46:18 +0100

    Refactor: Relocate color, line cap, and line join definitions in DrawingController
    This commit moves the `colors`, `lineCaps`, and `lineJoins` array definitions out of the `DrawingController` class and into the top level of the file.
    
    - **`feature/drawing/src/main/java/com/mshdabiola/drawing/DrawingController.kt`**:
        - The `colors`, `lineCaps`, and `lineJoins` arrays are now defined as top-level constants.
        - The `DrawingController` class remains, but no longer contains these definitions.

--- feature/drawing/src/main/java/com/mshdabiola/drawing/DrawingController.kt
+++ feature/drawing/src/main/java/com/mshdabiola/drawing/DrawingController.kt
@@ -19,27 +19,28 @@ import kotlinx.collections.immutable.toImmutableMap
 
 typealias ImmutablePath = ImmutableMap<PathData, List<Coordinate>>
 
-@SuppressLint("MutableCollectionMutableState")
-class DrawingController {
-    val colors = arrayOf(
-        Color.Black,
-        Color.Red,
-        Color.Green,
-        Color.Blue,
-        Color.Magenta,
-        Color.Cyan,
-        Color.Yellow,
-        Color(0xFF651FFF),
-        Color(0xFFD500F9),
-        Color(0xFFFFEA00),
-        Color(0xFF1DE9B6),
-        Color(0xFFF50057),
-        Color(0xFFFF3D00),
+val colors = arrayOf(
+    Color.Black,
+    Color.Red,
+    Color.Green,
+    Color.Blue,
+    Color.Magenta,
+    Color.Cyan,
+    Color.Yellow,
+    Color(0xFF651FFF),
+    Color(0xFFD500F9),
+    Color(0xFFFFEA00),
+    Color(0xFF1DE9B6),
+    Color(0xFFF50057),
+    Color(0xFFFF3D00),
 
     )
 
-    val lineCaps = arrayOf(StrokeCap.Round, StrokeCap.Butt, StrokeCap.Round)
-    val lineJoins = arrayOf(StrokeJoin.Round, StrokeJoin.Bevel, StrokeJoin.Miter)
+val lineCaps = arrayOf(StrokeCap.Round, StrokeCap.Butt, StrokeCap.Round)
+val lineJoins = arrayOf(StrokeJoin.Round, StrokeJoin.Bevel, StrokeJoin.Miter)
+
+@SuppressLint("MutableCollectionMutableState")
+class DrawingController {
 
     var lineWidth = 8
     var lineCap = 0
