97050235348adafe022e1173e5f2a81eb918e9de
mshdabiola <mshdabiola@gmail.com>
2025-07-02 03:11:46 +0100

    Refactor: Remove exclusion of protolite-well-known-types
    This commit removes the explicit exclusion of the `com.google.firebase:protolite-well-known-types` dependency from `firebase-bom` and `firebase-analytics` in the `googlePlay` build variant.
    
    This implies that the dependency is now either accepted, or excluded by other means.
    
    **Key changes:**
    
    *   **`modules/analytics/build.gradle.kts`**:
        *   Removed `exclude(group = "com.google.firebase", module = "protolite-well-known-types")` from the `googlePlayImplementation` of `libs.firebase.bom`.
        *   Removed `exclude(group = "com.google.firebase", module = "protolite-well-known-types")` from the `googlePlayImplementation` of `libs.firebase.analytics`.

--- modules/analytics/build.gradle.kts
+++ modules/analytics/build.gradle.kts
@@ -17,11 +17,7 @@ dependencies {
     implementation(libs.androidx.compose.runtime)
 
     //  prodImplementation(platform(libs.firebase.bom))
-    googlePlayImplementation(platform(libs.firebase.bom)){
-        exclude(group = "com.google.firebase", module = "protolite-well-known-types")
-    }
+    googlePlayImplementation(platform(libs.firebase.bom))
 
-    googlePlayImplementation(libs.firebase.analytics){
-        exclude(group = "com.google.firebase", module = "protolite-well-known-types")
-    }
+    googlePlayImplementation(libs.firebase.analytics)
 }
