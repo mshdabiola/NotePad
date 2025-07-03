ad2a9bdd7edb8878853d74494e09cb98d85d2740
mshdabiola <mshdabiola@gmail.com>
2025-07-03 04:49:30 +0100

    Docs: Update MainScreen screenshot tests and add reference images
    This commit refactors the screenshot tests for `MainScreen` and adds new reference images for various states and configurations.
    
    **Key changes:**
    
    *   **`feature/main/src/screenshotTest/kotlin/com/mshdabiola/main/MainScreenScreenshotTests.kt`**:
        *   Wrapped screenshot test functions within the `MainScreenScreenshotTests` class.
        *   Renamed `MainListScreenShot` to `Main`.
        *   Renamed `MainLoadingScreenShot` to `Loading`.
        *   Replaced `@PreviewAllLocales` with `@PreviewMain` for all screenshot previews.
        *   Removed unused import `com.mshdabiola.testing.util.PreviewAllLocales`.
        *   Added import `com.mshdabiola.ui.PreviewMain`.
    
    *   **`feature/main/src/fossReliantDebug/screenshotTest/reference/com/mshdabiola/main/MainScreenScreenshotTests/`**:
        *   Added new reference images for the `Loading` state:
            *   `Loading_Day_d34ad6fb_0.png`
            *   `Loading_English_cce9027c_0.png`
            *   `Loading_French_1c702e6d_0.png`
            *   `Loading_Night_afcc8c39_0.png`
            *   `Loading_Russian_3995260b_0.png`
        *   Added new reference images for the `Main` (list) state:
            *   `Main_Day_d34ad6fb_0.png`
            *   `Main_English_cce9027c_0.png`
            *   `Main_French_1c702e6d_0.png`
            *   `Main_Night_afcc8c39_0.png`
            *   `Main_Russian_3995260b_0.png`
        *   Added new reference images for the `MainEmptyScreenShot` state:
            *   `MainEmptyScreenShot_Day_d34ad6fb_0.png`
            *   `MainEmptyScreenShot_English_cce9027c_0.png`
            *   `MainEmptyScreenShot_French_1c702e6d_0.png`
            *   `MainEmptyScreenShot_Night_afcc8c39_0.png`
            *   `MainEmptyScreenShot_Russian_3995260b_0.png`
    
    *   **`feature/main/src/fossReliantDebug/screenshotTest/reference/com/mshdabiola/main/MainScreenScreenshotTestsKt/MainScreenShot_0.png`**:
        *   Deleted this old reference image as tests are now organized within a class.

--- feature/main/src/screenshotTest/kotlin/com/mshdabiola/main/MainScreenScreenshotTests.kt
+++ feature/main/src/screenshotTest/kotlin/com/mshdabiola/main/MainScreenScreenshotTests.kt
@@ -8,54 +8,57 @@ import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.runtime.Composable
 import com.mshdabiola.model.NoteDisplayCategory
 import com.mshdabiola.model.getDefinedNotePads
-import com.mshdabiola.testing.util.PreviewAllLocales
 import com.mshdabiola.ui.PreviewContainer
+import com.mshdabiola.ui.PreviewMain
 
-@OptIn(ExperimentalSharedTransitionApi::class)
-@PreviewAllLocales
-@Composable
-fun MainListScreenShot() {
-    val list = getDefinedNotePads()
-    val pin = list.take(3)
-    val unPin = list.takeLast(7)
+class MainScreenScreenshotTests {
 
-    PreviewContainer {
-        MainScreen(
-            mainState = MainState.Success(
-                isGrid = true,
-                labelName = "Label",
-                pinNotePads = pin,
-                unPinNotePads = unPin,
-                noteDisplayCategory = NoteDisplayCategory(),
-                selectState = null,
-            ),
-        )
+    @OptIn(ExperimentalSharedTransitionApi::class)
+    @PreviewMain
+    @Composable
+    fun Main() {
+        val list = getDefinedNotePads()
+        val pin = list.take(3)
+        val unPin = list.takeLast(7)
+
+        PreviewContainer {
+            MainScreen(
+                mainState = MainState.Success(
+                    isGrid = true,
+                    labelName = "Label",
+                    pinNotePads = pin,
+                    unPinNotePads = unPin,
+                    noteDisplayCategory = NoteDisplayCategory(),
+                    selectState = null,
+                ),
+            )
+        }
     }
-}
 
-@OptIn(ExperimentalSharedTransitionApi::class)
-@PreviewAllLocales
-@Composable
-fun MainEmptyScreenShot() {
-    PreviewContainer {
-        MainScreen(
-            mainState = MainState.Success(
-                isGrid = true,
-                labelName = "Label",
-                noteDisplayCategory = NoteDisplayCategory(),
-                selectState = null,
-            ),
-        )
+    @OptIn(ExperimentalSharedTransitionApi::class)
+    @PreviewMain
+    @Composable
+    fun MainEmptyScreenShot() {
+        PreviewContainer {
+            MainScreen(
+                mainState = MainState.Success(
+                    isGrid = true,
+                    labelName = "Label",
+                    noteDisplayCategory = NoteDisplayCategory(),
+                    selectState = null,
+                ),
+            )
+        }
     }
-}
 
-@OptIn(ExperimentalSharedTransitionApi::class)
-@PreviewAllLocales
-@Composable
-fun MainLoadingScreenShot() {
-    PreviewContainer {
-        MainScreen(
-            mainState = MainState.Loading,
-        )
+    @OptIn(ExperimentalSharedTransitionApi::class)
+    @PreviewMain
+    @Composable
+    fun Loading() {
+        PreviewContainer {
+            MainScreen(
+                mainState = MainState.Loading,
+            )
+        }
     }
 }
